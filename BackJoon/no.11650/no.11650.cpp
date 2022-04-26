cddcdf082f5575fdcba794febfbd67a341313a0e
YOUNGHOON1010 <82190412+yunghoon19@users.noreply.github.com>
2022-04-26 19:38:53 +0900

    Backjoon_Algorithm_11650
    11650 백준 알고리즘문제 해결

---
+++ BackJoon/no.11650/no.11650.cpp
@@ -0,0 +1,54 @@
+/*
+    백준 알고리즘 문제 11650번 소스 코드
+*/
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX 100000
+
+struct Location
+{
+    int x, y;
+};
+
+bool Compare(Location& _l1, Location& _l2)
+{
+    if (_l1.x == _l2.x)
+    {
+        return _l1.y < _l2.y;
+    }
+    else
+    {
+        return _l1.x < _l2.x;
+    }
+}
+
+int main(void)
+{
+    int N;
+    Location *lArr;
+
+    scanf("%d", &N);
+
+    lArr = new Location[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d %d", &lArr[i].x, &lArr[i].y);
+    }
+
+    sort(lArr, lArr + N, Compare);
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d %d\n", lArr[i].x, lArr[i].y);
+    }
+
+    delete[] lArr;
+
+    return 0;
+}
