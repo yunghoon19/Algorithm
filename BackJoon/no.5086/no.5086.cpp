3c8b24af1e34f830c0bc712677282a5c391f97b3
YOUNGHOON1010 <82190412+yunghoon19@users.noreply.github.com>
2022-05-01 20:08:17 +0900

    백준 5086 알고리즘문제
    해결

---
+++ BackJoon/no.5086/no.5086.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+
+    while (true)
+    {
+        cin >> a >> b;
+
+        if (a + b != 0)
+        {
+            // 첫번째 숫자가 두번째 숫자의
+            if (b % a == 0)         // 약수
+            {
+                cout << "factor" << endl;
+            }
+            else if (a % b == 0)    // 배수
+            {
+                cout << "multiple" << endl;
+            }
+            else                    // 둘 다 아님
+            {
+                cout << "neither" << endl;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
