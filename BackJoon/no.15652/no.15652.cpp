c28e4c873772decdec3e0f0fa36ddeef4dfd05ae
YOUNGHOON1010 <82190412+yunghoon19@users.noreply.github.com>
2022-05-16 18:57:14 +0900

    15652 백준 알고리즘문제
    해결

---
+++ BackJoon/no.15652/no.15652.cpp
@@ -0,0 +1,44 @@
+/*
+    백준 알고리즘 문제 15652번 소스 코드
+*/
+
+#include <iostream>
+
+using namespace std;
+
+#define MAX 9
+
+int resultArr[MAX];
+bool chackVis[MAX] = {false, };
+
+void BT(int _depth, int _cnt ,const int& _N, const int& _M)
+{
+    if (_depth == _M)
+    {
+        for (int i = 0; i < _M; i++)
+        {
+            printf("%d ", resultArr[i] + 1);
+        }
+        printf("\n");
+        return;
+    }
+
+    for (int i = 0; i < _N; i++)
+    {
+        if (_cnt <= i)
+        {
+            resultArr[_depth] = i;
+            BT(_depth + 1, i, _N, _M);
+        }
+    }
+}
+
+int main()
+{
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    BT(0, 0, N, M);
+
+    return 0;
+}
